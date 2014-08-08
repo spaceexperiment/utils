23a50d49fe3340aa86ad121d5cdf980c72bcfbc9
jibreel <jibbo88@gmail.com>
2014-08-08 14:01:10 +0300

    only hash files that have one or more equel file with same size

--- duplicates.py
+++ duplicates.py
@@ -23,16 +23,24 @@ def md5_file(file_path, block_size=2**20 * 60):
     return m.digest()
 
 
-files = defaultdict(list)
+file_sizes = defaultdict(list)
 for root, dirs, filenames in os.walk(path):
     for filename in filenames:
         file_path = os.path.join(root, filename)
-        key = md5_file(file_path)
-        files[key].append(file_path)
+        file_sizes[os.path.getsize(file_path)].append(file_path)
+# list of files with one or more equel file size
+file_sizes = [x for x in file_sizes.values() if len(x) > 1]
 
+files = defaultdict(list)
+for paths in file_sizes:
+    for path in paths:
+        key = md5_file(path)
+        files[key].append(path)
 files = [x for x in files.values() if len(x) > 1]
+
 for duplicates in files:
     print '\n\n_________________________________________'
     pprint(duplicates)
     print '_________________________________________'
+
 print '\n%s files with atleast one duplicate\n' % len(files)
